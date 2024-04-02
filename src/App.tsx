4493491f9927b537354d135faeb65a012fbecff7
josefaidt <josef.aidt@gmail.com>
2024-04-02 13:06:41 -0700

    added create, list, and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import type { Schema } from "../amplify/data/resource"
+import { useEffect, useState } from "react"
+import { generateClient } from "aws-amplify/data"
+
+const client = generateClient<Schema>()
 
 function App() {
-  const [todos, setTodos] = useState<Array<{ id: number, content: string }>>([])
+  const [todos, setTodos] = useState<Array<Schema["Todo"]>>([])
+
+  useEffect(() => {
+    client.models.Todo.observeQuery().subscribe({
+      next: (data) => setTodos([...data.items]),
+    })
+  }, [])
 
   function createTodo() {
-    setTodos([...todos, { id: todos.length, content: window.prompt("Todo content") ?? "" }])
+    client.models.Todo.create({ content: window.prompt("Todo content") })
+  }
+
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
   }
 
   return (
@@ -12,15 +26,17 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => <li
-          key={todo.id}>
+        {todos.map((todo) => (
+          <li key={todo.id}>
             {todo.content}
-          </li>)}
-      </ul> 
+            <button onClick={() => deleteTodo(todo.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="">Review next step of this tutorial.</a>
+        <a href="#">Review next step of this tutorial.</a>
       </div>
     </main>
   )
